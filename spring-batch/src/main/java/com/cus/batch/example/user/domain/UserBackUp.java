822397155dc1c8782178a73184b4e06e4fc3f889
ungseok <cus4703@gmail.com>
2022-09-21 13:16:56 +0900

    feat: retry skip code

---
+++ spring-batch/src/main/java/com/cus/batch/example/user/domain/UserBackUp.java
@@ -0,0 +1,31 @@
+package com.cus.batch.example.user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Builder
+@Entity
+@Table(name = "user_backup")
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserBackUp {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  private String name;
+  private int age;
+
+  public UserBackUp(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+}
