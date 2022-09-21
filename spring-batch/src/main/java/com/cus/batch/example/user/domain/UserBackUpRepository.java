cd5a2b234761cf1a7c61b3c32b2925dc4d5cb869
ungseok <cus4703@gmail.com>
2022-09-21 11:59:00 +0900

    remove: 브랜치 이동하기 위해 삭제

--- spring-batch/src/main/java/com/cus/batch/example/user/domain/UserBackUpRepository.java
+++
@@ -1,6 +0,0 @@
-package com.cus.batch.example.user.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserBackUpRepository extends JpaRepository<UserBackUp, Long> {
-}
--- spring-batch/src/main/java/com/cus/batch/example/user/domain/UserHistoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.cus.batch.example.user.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserHistoryRepository extends JpaRepository<UserHistory, Long> {
-}
