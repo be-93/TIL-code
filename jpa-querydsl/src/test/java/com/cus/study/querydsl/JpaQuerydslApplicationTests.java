cdc2385c954774a3b6711dc8243c0067a53deeeb
ungseok <cus4703@gmail.com>
2022-09-28 19:25:15 +0900

    feat: querydsl study start

---
+++ jpa-querydsl/src/test/java/com/cus/study/querydsl/JpaQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cus.study.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaQuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
